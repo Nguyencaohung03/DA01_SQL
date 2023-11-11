e04b65a9f330985b5869a2ac61119a625bb2f2f2
Nguyencaohung03 <150541530+Nguyencaohung03@users.noreply.github.com>
2023-11-12 01:09:46 +0700

    Update Day3_ex.sql

--- Day3_ex.sql
+++ Day3_ex.sql
@@ -1,1 +1,18 @@
-select * from...
+-- ex1 
+select NAME from CITY where POPULATION > 120000 and COUNTRYCODE = 'USA'
+-- ex2
+select * from CITY where  COUNTRYCODE = 'JPN'
+-- ex3
+select CITY, STATE  from STATION
+-- ex4 
+select CITY from STATION where CITY like 'A%' or CITY like 'E%' or CITY like 'I%' or CITY like 'O%' or CITY like 'U%'
+--ex5
+select distinct CITY from STATION where CITY like '%A' or CITY like '%E' or CITY like '%I' or CITY like '%O' or CITY like '%U'
+--ex6
+select distinct CITY from STATION where CITY not like 'A%' and CITY not like 'E%' and CITY not like 'I%' 
+and CITY not like 'O%' and CITY not like 'U%'
+--ex7
+
+
+
+
