7ab9ae43e3b63ff2f49be5fbce6eb66946f03bc1
Nguyencaohung03 <150541530+Nguyencaohung03@users.noreply.github.com>
2023-11-16 01:46:33 +0700

    Update DA01_day7.sql

--- DA01_day7.sql
+++ DA01_day7.sql
@@ -1,1 +1,19 @@
 --ex1 
+select Name
+from STUDENTS
+where Marks > 75
+Order BY Right(Name, 3), ID
+--ex2
+
+  
+--ex3
+
+
+--ex4
+SELECT 
+extract(month from submit_date) as mth, 
+product_id as product,
+round(AVG(stars), 2) as avg_stars
+FROM reviews
+group by product_id, extract(month from submit_date)
+order by extract(month from submit_date), product_id
