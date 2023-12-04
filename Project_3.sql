a18370f136a5379f1981e046556503ef6bc872eb
Nguyencaohung03 <150541530+Nguyencaohung03@users.noreply.github.com>
2023-12-05 01:07:32 +0700

    Update Project_3.sql

--- Project_3.sql
+++ Project_3.sql
@@ -33,6 +33,39 @@ group by year_id,productline
   --Classic Cars
 --5.Khách hàng tốt nhất, phân tích dựa vào RFM 
 
+With customer_RFM as
+(
+select 
+a.customer_id,
+current_date - Max(order_date)as R,
+count(distinct b.order_id)as F,
+sum(sales)as M
+from customer a
+join sales b on a.customer_id=b.customer_id
+group by a.customer_id
+)
+,rfm_score as
+(
+select 
+customer_id,
+ntile(5)over(order by R DESC)as r_score,
+ntile(5)over(order by F)as f_score,
+ntile(5)over(order by M)as m_score
+from customer_RFM
+)
 
+,rfm_final as
+(
+select customer_id,
+Cast(r_score as VARCHAR) || Cast(f_score as VARCHAR)  || Cast(m_score as VARCHAR)  as rfm_score
+from rfm_score
+)
 
+select segment,count(*) from
+(select 
+a.customer_id, b.segment
+from rfm_final a
+join segment_score b on a.rfm_score=b.scores) as a
+group by segment
+order by count(*)
 
