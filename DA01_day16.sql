ea3d75e5878b3066c927ebab854db481eb819330
Nguyencaohung03 <150541530+Nguyencaohung03@users.noreply.github.com>
2023-12-12 23:57:46 +0700

    Update DA01_day16.sql

--- DA01_day16.sql
+++ DA01_day16.sql
@@ -49,9 +49,50 @@ round(avg(total) over(order by visited_on rows 6 preceding), 2) AS average_amoun
 from twt
 Limit 100 offset 6
 --ex5
-
+with cte as 
+(select 
+tiv_2015, tiv_2016, CONCAT(lat,lon) as  location,
+count(*)over(partition by concat(lat,lon)) as location_count,
+count(tiv_2015)over(partition by tiv_2015) as count_tiv_2015
+from Insurance
+) 
+select round(SUM(total), 2) as tiv_2016
+from(
+select tiv_2015, sum(tiv_2016) as total
+from cte
+where count_tiv_2015 >1 and location_count = 1
+group by tiv_2015
+) as a
 --ex6
+with cte1 as 
+(select a.id, Department, Employee,Salary
+from(
+select
+a.id,
+b.name as Department, a.name as Employee, a.salary as Salary, 
+Dense_rank()over(order by salary DESC) as stt
+from Employee as a
+join Department b on a.departmentId=b.id
+where a.departmentId =1) a
+where stt <=3)
+,cte2 as
+(select a.id, Department, Employee,Salary
+from(
+select
+a.id,
+b.name as Department, a.name as Employee, a.salary as Salary, 
+Dense_rank()over(order by salary DESC) as stt
+from Employee as a
+join Department b on a.departmentId=b.id
+where a.departmentId =2) a
+where stt <=3)
 
+select Department, Employee, Salary
+from cte1
+union all
+select Department, Employee, Salary
+from cte2
+    --đề yều cầu sắp xếp theo id nhưng e 'order by id' thì bị lỗi, anh check cho e nhé
 --ex7
 
 --ex8
