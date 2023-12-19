283ffb208085593896b6a13dd93e3c86832e06a5
Nguyencaohung03 <150541530+Nguyencaohung03@users.noreply.github.com>
2023-12-20 00:13:10 +0700

    Update HR.sql

--- HR.sql
+++ HR.sql
@@ -26,3 +26,16 @@ where (CITY LIKE 'a%'
     OR CITY LIKE '%o'
     OR CITY LIKE '%u'
 )
+/* define an employee's total earnings to be their monthly  worked, 
+  and the maximum total earnings to be the maximum total earnings for any employee in the Employee table. 
+  Write a query to find the maximum total earnings for all employees as well as the total number of employees who have maximum total earnings. 
+  Then print these values as  space-separated integers.
+*/
+select (months * salary) as earnings,
+count(*) 
+from Employee 
+group by earnings 
+order by earnings desc
+limit 1
+
+
