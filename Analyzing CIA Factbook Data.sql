f47cf9a367411eca573070c99541336ed66d7c73
Nguyencaohung03 <150541530+Nguyencaohung03@users.noreply.github.com>
2024-03-05 00:40:45 +0700

    Create Analyzing CIA Factbook Data .sql

---
+++ Analyzing CIA Factbook Data.sql
@@ -0,0 +1,13 @@
+--Write a query to return information on the tables in the database.
+--write and run another query that returns the first five rows of the facts table in the database.
+SELECT * FROM facts
+LIMIT 5
+-- Write a single query that returns the following:
+--Minimum population
+
+--Maximum population
+
+--Minimum population growth
+
+--Maximum population growth 
+
